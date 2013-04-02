9171ba50ee0f280437810f664a72fcada9e0338b
Domingo Ernesto Savoretti <esavoretti@gmail.com>
2013-04-02 02:32:08 -0300

    working on makeserver...

--- compile.sh
+++ compile.sh
@@ -5,3 +5,9 @@ echo " " >> lib/test_router.tmp
 cat lib/test_router.js >> lib/test_router.tmp
 mv lib/test_router.tmp lib/test_router.js
 chmod +x lib/test_router.js
+echo "#!/usr/bin/env node" > bin/mk-server
+echo " " >> bin/mk-server
+cat lib/mk-server.js >> bin/mk-server
+chmod +x bin/mk-server
+rm lib/mk-server.js
+
--- src/router.coffee
+++ src/router.coffee
@@ -67,7 +67,7 @@ Router = (options = {}) ->
     cgi_dir: "cgi-bin"
     serve_cgi: true
     served_by: 'Node Simple Router'
-    version: '0.2.6'
+    version: '0.2.7'
 
 # End of Constants.
 
