7dfcef002691f624c7c73a15655afb3b88e56e43
Domingo Ernesto Savoretti <esavoretti@gmail.com>
2013-04-03 18:21:54 -0300

    added ability to handle form uploads

--- compile.sh
+++ compile.sh
@@ -1,13 +1,23 @@
 coffee -c -o lib/ src/
-coffee -c -o lib/ test/
-echo "#!/usr/bin/env node" > lib/test_router.tmp
-echo " " >> lib/test_router.tmp
-cat lib/test_router.js >> lib/test_router.tmp
-mv lib/test_router.tmp lib/test_router.js
-chmod +x lib/test_router.js
+coffee -c test/
+
+echo "#!/usr/bin/env node" > test/common/test_router.tmp
+echo " " >> test/common/test_router.tmp
+cat test/common/test_router.js >> test/common/test_router.tmp
+mv test/common/test_router.tmp test/common/test_router.js
+chmod +x test/common/test_router.js
+
+
+echo "#!/usr/bin/env node" > test/uploader/server.tmp
+echo " " >> test/uploader/server.tmp
+cat test/uploader/server.js >> test/uploader/server.tmp
+mv test/uploader/server.tmp test/uploader/server.js
+chmod +x test/uploader/server.js
+
 echo "#!/usr/bin/env node" > bin/mk-server
 echo " " >> bin/mk-server
 cat lib/mk-server.js >> bin/mk-server
 chmod +x bin/mk-server
+
 rm lib/mk-server.js
 
