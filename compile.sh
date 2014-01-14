534b2a465901f8b72d8eb9d93ad8bbed8fc71de0
Domingo Ernesto Savoretti <esavoretti@gmail.com>
2014-01-14 05:16:10 -0300

    v0.5.0-2

--- compile.sh
+++ compile.sh
@@ -1,19 +1,12 @@
 coffee -c -o lib/ src/
 coffee -c test/
 
-echo "Generating test_router.js"
-echo "#!/usr/bin/env node" > test/common/test_router.tmp
-echo " " >> test/common/test_router.tmp
-cat test/common/test_router.js >> test/common/test_router.tmp
-mv test/common/test_router.tmp test/common/test_router.js
-chmod +x test/common/test_router.js
-
-echo "Generating uploader/server.js"
-echo "#!/usr/bin/env node" > test/uploader/server.tmp
-echo " " >> test/uploader/server.tmp
-cat test/uploader/server.js >> test/uploader/server.tmp
-mv test/uploader/server.tmp test/uploader/server.js
-chmod +x test/uploader/server.js
+echo "Generating test/server.js"
+echo "#!/usr/bin/env node" > test/server.tmp
+echo " " >> test/server.tmp
+cat test/server.js >> test/server.tmp
+mv test/server.tmp test/server.js
+chmod +x test/server.js
 
 echo "Generating mk-server"
 echo "#!/usr/bin/env node" > bin/mk-server
